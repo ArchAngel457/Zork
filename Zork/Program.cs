36eb0aac088e118ed585b62eac3af2523096d856
ArchAngel457 <47088320+ArchAngel457@users.noreply.github.com>
2020-10-01 17:58:50 -0400

    Zork 3.1 (Help)

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Zork
 {
-    internal class Program
+    class Program
     {
-        private static string CurrentRoom
-        {
-            get
-            {
-                return Rooms[Location.Row, Location.Column]; //why the error?
-            }
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -20,82 +12,32 @@ namespace Zork
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
-                Console.WriteLine(CurrentRoom);
                 Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
+                command = ToCommand(Console.ReadLine());
 
+                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        Console.WriteLine("Thank you for playing!");
+                        outputString = "Thanks for playing!";
                         break;
-
                     case Commands.LOOK:
-                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.";
                         break;
-
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        if (Move(command) == false)
-                        {
-                            Console.WriteLine("The way is shut!");
-                        }
+                        outputString = $"You moved {command}";
                         break;
-
                     default:
-                        Console.WriteLine("Unkown command.");
+                        outputString = "Unknown Command";
                         break;
                 }
+                Console.WriteLine(outputString);
             }
         }
 
-        private static bool Move(Commands command)
-        {
-            //Assert.IsTrue(IsDirection(command), "Invalid direction."); 
-
-            bool isValidMove = true;
-            switch (command)
-            {
-                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
-                    Location.Row++;
-                    break;
-
-                case Commands.SOUTH when Location.Row > 0:
-                    Location.Row--;
-                    break;
-
-                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-                    Location.Column++;
-                    break;
-
-                case Commands.WEST when Location.Column > 0:
-                    Location.Column--;
-                    break;
-
-                default:
-                    isValidMove = false;
-                    break;
-            }
-
-            return isValidMove;
-        }
-
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
-        private static bool isDirection(Commands command) => Directions.Contains(command);
-
-        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST
-        };
-
-        private static (int Row, int Column) Location = (1,1);
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
     }
 }
